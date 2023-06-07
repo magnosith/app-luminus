9f508a0acac11c2eebae12af4b9d71c8af15f22f
magnosith <magnosith@icloud.com>
2023-06-07 16:20:58 -0700

    Initial Commit

---
+++ app-luminus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-luminus
+//
+//  Created by Student on 07/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
