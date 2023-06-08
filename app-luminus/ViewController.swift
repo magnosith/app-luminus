f356cc5659ba10a224d031461cc3a057c27b403f
magnosith <magnosith@icloud.com>
2023-06-07 17:25:56 -0700

    Comiit Final

--- app-luminus/ViewController.swift
+++ app-luminus/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textStatusTitle: UILabel!
+    @IBOutlet weak var imageWand: UIImageView!
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func updateUiLight() {
+        if lightOn {
+            view.backgroundColor = .white
+            textStatusTitle.text = "Nox"
+            textStatusTitle.textColor = .black
+        } else {
+            view.backgroundColor = .black
+            textStatusTitle.text = "Lumus"
+            textStatusTitle.textColor = .white
+        }
+    }
+    
+    @IBAction func turnOnOff(_ sender: UITapGestureRecognizer) {
+        lightOn.toggle()
+        updateUiLight()
+    }
+    
 }
 
